9d01cd007ad5a49bbbd7cbab3e2cc0728336db58
Wojtek-Rokicki <rokwojtek@gmail.com>
2018-03-20 23:08:23 +0100

    Update no.4 - Dziala!!!

--- library.cpp
+++ library.cpp
@@ -1,32 +1,34 @@
 #include "library.h"
-
 #include <iostream>
+#include <unordered_map>
+using namespace std;
 
-SparseVector::SparseVector(){
+SparseVector::SparseVector() {
 
 }
 
 //Przypisanie wartosci z danego obiektu na rzecz obiektu ktorego zostal wywolany operator
 void SparseVector::operator=(const SparseVector &v1){
+    this->svector.clear();
     for(auto& x: v1.svector){
-        this->svector.insert(x);
+        this->svector[x.first] = x.second;
     }
 }
-
 //Jesli w wektorze, na rzecz ktorego jest wywolywany operator, nie ma na danej pozycji wartosci to dodaj pare,
 //w przeciwnym wypadku dodaj wartosci
 SparseVector SparseVector::operator+(const SparseVector &v1){
     SparseVector sum;
     sum=*this;
+
     for(auto& x: v1.svector){
-        std::unordered_map<int, double>::iterator y=sum.svector.find(x.first);
-        if(sum.svector.end() == y){
+        std::unordered_map<int,double>::const_iterator y = sum.svector.find(x.first);
+        if(y == sum.svector.end()){
             sum.svector.insert(x);
         }else{
-            std::pair <int, double> p  (x.first,sum.svector.at(x.first)+x.second);
-            sum.svector.insert(p);
+            sum.svector[x.first] = sum.svector[x.first] + x.second;
         }
     }
+    return sum;
 }
 
 void SparseVector::operator+=(const SparseVector &v1){
@@ -38,15 +40,13 @@ SparseVector SparseVector::operator-(const SparseVector &v1){
     sum=*this;
     for(auto& x: v1.svector){
         std::unordered_map<int, double>::const_iterator y=sum.svector.find(x.first);
-        std::unordered_map<int, double>::const_iterator z=sum.svector.end();
-        if(y==z){
-            std::pair <int, double> p  (x.first,-x.second);
-            sum.svector.insert(p);
+        if(y == sum.svector.end()){
+            sum.svector[x.first] = -x.second;
         }else{
-            std::pair <int, double> p  (x.first,sum.svector.at(x.first)-x.second);
-            sum.svector.insert(p);
+            sum.svector[x.first] = sum.svector[x.first] - x.second;
         }
     }
+    return sum;
 }
 
 void SparseVector::operator-=(const SparseVector &v1){
--- main.cpp
+++ main.cpp
@@ -4,29 +4,31 @@
 #include <iostream>
 #include "library.h"
 
-int main(){
+using namespace std;
 
+int main() {
     SparseVector wektor1;
-    wektor1.svector[0]=3.45;
-    wektor1.svector[1]=8.66;
-    wektor1.svector[2]=5.22;
-    wektor1.svector[29]=6;
-
+    wektor1.svector[0] = 3.45;
+    wektor1.svector[1] = 8.66;
+    wektor1.svector[2] = 5.22;
+    wektor1.svector[28] = 6;
     wektor1.show();
 
+    std::cout << std::endl;
     SparseVector wektor2;
-    wektor2.svector[0]=3.45;
-    wektor2.svector[1]=8.66;
-    wektor2.svector[2]=5.22;
-    wektor2.svector[29]=6;
-
+    wektor2.svector[0] = 1.45;
+    wektor2.svector[1] = 66;
+    wektor2.svector[2] = 5.2;
+    wektor2.svector[29] = 3;
     wektor2.show();
 
+    std::cout << std::endl;
     SparseVector wektor3;
-
-    wektor3=wektor1+wektor2;
-
+    wektor3 = wektor2 - wektor1;
+    cout << 7 << endl;
     wektor3.show();
+    cout << 8 << endl;
+    std::cout << std::endl;
 
     return 0;
 }
