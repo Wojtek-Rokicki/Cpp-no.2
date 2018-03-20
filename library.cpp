50535895f0d0cd2ce88c96b2be21277196d7dfd6
Wojtek-Rokicki <rokwojtek@gmail.com>
2018-03-20 18:50:13 +0100

    Update no.2 (problem z porównaniem iteratorów)

--- library.cpp
+++ library.cpp
@@ -7,7 +7,7 @@ SparseVector::SparseVector(){
 }
 
 //Przypisanie wartosci z danego obiektu na rzecz obiektu ktorego zostal wywolany operator
-void operator=(const SparseVector &v1){
+void SparseVector::operator=(const SparseVector &v1){
     for(auto& x: v1.svector){
         this->svector.insert(x);
     }
@@ -19,11 +19,12 @@ SparseVector SparseVector::operator+(const SparseVector &v1){
     SparseVector sum;
     sum=*this;
     for(auto& x: v1.svector){
-        std::unordered_map::const_iterator y=x.first;
-        if(sum.svector.find(y)==sum.svector.end()){
+        std::unordered_map<int, double>::iterator y=sum.svector.find(x.first);
+        if(sum.svector.end() == y){
             sum.svector.insert(x);
         }else{
-            sum.svector.insert(sum.svector.at(x.first)+x.second);
+            std::pair <int, double> p  (x.first,sum.svector.at(x.first)+x.second);
+            sum.svector.insert(p);
         }
     }
 }
@@ -36,12 +37,14 @@ SparseVector SparseVector::operator-(const SparseVector &v1){
     SparseVector sum;
     sum=*this;
     for(auto& x: v1.svector){
-        std::unordered_map::const_iterator y=x.first;
-        if(sum.svector.find(y)==sum.svector.end()){
-            sum.svector.insert(x);
-            sum.svector.(x.first).second=-(v1.svector.(x.first).second);
+        std::unordered_map<int, double>::const_iterator y=sum.svector.find(x.first);
+        std::unordered_map<int, double>::const_iterator z=sum.svector.end();
+        if(y==z){
+            std::pair <int, double> p  (x.first,-x.second);
+            sum.svector.insert(p);
         }else{
-            sum.svector.insert(sum.svector.at(x.first)+x.second);
+            std::pair <int, double> p  (x.first,sum.svector.at(x.first)-x.second);
+            sum.svector.insert(p);
         }
     }
 }
@@ -60,6 +63,6 @@ void SparseVector::operator*=(const SparseVector &v1){
 
 void SparseVector::show(){
     for(auto& x: this->svector){
-        std::cout<<this->svector.(x.first).first<<" : "<<this->svector.(x.first).second<<std::endl;
+        std::cout<<x.first<<" : "<<x.second<<std::endl;
     }
 }
--- main.cpp
+++ main.cpp
@@ -12,6 +12,22 @@ int main(){
     wektor1.svector[2]=5.22;
     wektor1.svector[29]=6;
 
+    wektor1.show();
+
+    SparseVector wektor2;
+    wektor2.svector[0]=3.45;
+    wektor2.svector[1]=8.66;
+    wektor2.svector[2]=5.22;
+    wektor2.svector[29]=6;
+
+    wektor2.show();
+
+    SparseVector wektor3;
+
+    wektor3=wektor1+wektor2;
+
+    wektor3.show();
+
     return 0;
 }
 
